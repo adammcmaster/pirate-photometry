3ecaf6dd5a28e0a84c66ea80b575b4418c19161e
Adam <anm83@tatsuo.open.ac.uk>
2021-12-09 13:40:56 +0000

    Formatting

--- targets.py
+++ targets.py
@@ -1,38 +1,20 @@
 from datetime import date
 import re
 
-ALL_FILTERS = {'U', 'B', 'V', 'R', 'I'}
+ALL_FILTERS = {"U", "B", "V", "R", "I"}
 
 ALL_TELESCOPE_FILTERS = {
-    'PIRATE': ALL_FILTERS,
-    'COAST': ALL_FILTERS - {'U', 'I'},
+    "PIRATE": ALL_FILTERS,
+    "COAST": ALL_FILTERS - {"U", "I"},
 }
 
 TARGETS = {
-    'HD38451': {
-        'end': date(2022, 1, 1),
-    },
-    '27Cephei': {
-        'end': date(2021, 11, 9),
-    },
-    '1SWASPJ002552.75+454445.3': {
-        'expected_periods': (24671.86133,),
-        'uses': '',
-    },
-    '1SWASPJ002552.88+454558.7': {
-        'expected_periods': (148009.10938,),
-        'uses': '',
-    },
-    '1SWASPJ184559.85+471858.4': {
-        'expected_periods': (37009.83594,),
-        'uses': '',
-    },
-    '1SWASPJ002551.12+454523.8': {
-        'expected_periods': (74014.22656,),
-        'uses': '',
-    },
+    "HD38451": {"end": date(2022, 1, 1),},
+    "27Cephei": {"end": date(2021, 11, 9),},
+    "1SWASPJ002552.75+454445.3": {"expected_periods": (24671.86133,), "uses": "",},
+    "1SWASPJ002552.88+454558.7": {"expected_periods": (148009.10938,), "uses": "",},
+    "1SWASPJ184559.85+471858.4": {"expected_periods": (37009.83594,), "uses": "",},
+    "1SWASPJ002551.12+454523.8": {"expected_periods": (74014.22656,), "uses": "",},
 }
 
-ESCAPED_TARGET_NAMES = {
-    re.sub(r'[.+-]', '_', name): name for name in TARGETS.keys()
-}
+ESCAPED_TARGET_NAMES = {re.sub(r"[.+-]", "_", name): name for name in TARGETS.keys()}
